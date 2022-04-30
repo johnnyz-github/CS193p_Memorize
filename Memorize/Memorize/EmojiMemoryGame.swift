b7675a2e6b2d4e428e8077809116800b37909888
johnnyz <johnny.zhou@covermore.com>
2022-05-01 09:57:39 +1000

    add model and viewmodel and init

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by JohnnyZ on 1/5/2022.
+//
+
+import Foundation
---
+++ Memorize/Memorize/MemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by JohnnyZ on 1/5/2022.
+//
+
+import Foundation
